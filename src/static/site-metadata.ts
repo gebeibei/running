1974cb6bd958c9577abd0a613b852924e67273bc
葛北北 <ge.beibei@cpe-smartcity.com>
2024-09-12 20:24:15 +0800

    信息修改

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,14 +11,14 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://gitee.com/gebeibei',
+  siteUrl: 'https://github.com/gebeibei',
   logo: 'https://foruda.gitee.com/avatar/1676955436582473300/1195579_gebeibei_1578945812.png!avatar200',
   description: 'Personal site and blog',
   navLinks: [
-    // {
-    //   name: 'Blog',
-    //   url: 'https://github.com/yihong0618/gitblog',
-    // },
+    {
+      name: 'Blog',
+      url: 'https://github.com/gebeibei/gitblog',
+    },
     // {
     //   name: 'About',
     //   url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
